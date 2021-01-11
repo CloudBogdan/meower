81665054f5ad77300944c47d0248d7105705250d
CloudBogdan <60233692+CloudBogdan@users.noreply.github.com>
2021-01-11 19:37:09 +0300

    .

--- utils/database.ts
+++ utils/database.ts
@@ -4,7 +4,7 @@ export async function connect() {
 
     console.log("Connected");
 
-    await mongoose.connect("mongodb+srv://bogdanoff:123RF4Ru43568@cluster0.badug.mongodb.net/main?retryWrites=true&w=majority", {
+    mongoose.connect("mongodb+srv://bogdanoff:123RF4Ru43568@cluster0.badug.mongodb.net/main?retryWrites=true&w=majority", {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
