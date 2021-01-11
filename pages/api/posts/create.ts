e2f26c3792a197c67c24d03e958d0c71d3519858
CloudBogdan <60233692+CloudBogdan@users.noreply.github.com>
2021-01-11 19:11:16 +0300

    Added error send

--- pages/api/posts/create.ts
+++ pages/api/posts/create.ts
@@ -7,16 +7,20 @@ export default async function(req: NextApiRequest, res: NextApiResponse) {
 
         new Post(JSON.parse(req.body.data)).save((err: any)=> {
 
-            if (err)
-                return console.error(err);
+            if (err) {
+                console.error(err);
+                res.status(500).json(err);
+                return
+            }
                 
-            res.send({ success: true });
+            res.status(200).send({ success: true });
 
         });
 
         
     } catch(err) {
         console.error(err);
+        res.status(500).json({ err });
     }
 
 } 
--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -12,9 +12,12 @@ export default async function(req: NextApiRequest, res: NextApiResponse) {
         
         if (posts)
             res.status(200).json(posts);
-
+        else
+            res.status(500);
+            
     } catch(err) {
         console.error(err);
+        res.status(500).json({ err });
     }
 
 }
